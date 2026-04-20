90f5f16510f3c6cbd68755806baf00adc93fff2e
Mykola <nyr3mvneov@gmail.com>
2026-04-20 21:01:13 +0300

    add DATABASE_URL support with SSL configuration

--- src/backend/src/db.js
+++ src/backend/src/db.js
@@ -1,15 +1,24 @@
+/* eslint-disable indent */
 'use strict';
 
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize({
-  dialect: 'postgres',
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT) || 5432,
-  database: process.env.DB_NAME,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  logging: false,
-});
+const sequelize = process.env.DATABASE_URL
+  ? new Sequelize(process.env.DATABASE_URL, {
+      dialect: 'postgres',
+      logging: false,
+      dialectOptions: {
+        ssl: { require: true, rejectUnauthorized: false },
+      },
+    })
+  : new Sequelize({
+      dialect: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT) || 5432,
+      database: process.env.DB_NAME,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      logging: false,
+    });
 
 module.exports = { sequelize };
